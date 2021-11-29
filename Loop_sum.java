ee20822ea1ed6bffa83ce9e655c1914bd83f0134
Shawn <93415785+llllSh5555llll@users.noreply.github.com>
2021-11-29 18:44:55 +0800

    Update Loop_sum.java

--- Loop_sum.java
+++ Loop_sum.java
@@ -2,14 +2,9 @@ public class Loop_sum {
     public static void main(String[] argv) {
       
         int i;
-        int j;
-        int x;
         int sum=0;
         for (i = 1 ; i <= 100; i++) {
-            j=i*i;
-            System.out.println(j);
-            for (x=1;x <= 100; x++) {
-                sum=sum+j;
+            sum+=i*i
             }
         }
         System.out.println(sum);
